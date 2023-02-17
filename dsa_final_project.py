ebead22cfa4e3756d0df490be7bb5a025f8a9d65
Carljhn <zoletacarljohn1@gmail.com>
2023-02-17 23:10:54 +0800

    Second commit: modified the codeblock for option A.

--- dsa_final_project.py
+++ dsa_final_project.py
@@ -27,61 +27,60 @@ def my_list():
     print(x.get_string(title="MY GROCERY LIST"))
     x.clear_rows()
 
-    Program = True
-    while Program:
-        user_input = input("\nWhat do you want to do? Please enter the corresponding letter of the options: ").lower()
+Program = True
+while Program:
+    user_input = input("\nWhat do you want to do? Please enter the corresponding letter of the options: ").lower()
+    
+    #option A
+    if user_input == "a":
+        new_item = input("\nWhat item do you want to add?: ").lower()
+        print("="*57)
+        print("An item has been added")
+        myList.append(new_item)
 
-        if user_input == "a":
-            new_item = input("What item do you want to add?: ")
-            myList.append(new_item)
+    elif user_input == "b":
+        itemName = input("Enter the item you want to remove: ")
+        if itemName in myList:
+            myList.remove(itemName)
             print("="*57)
-            print("An item has been added")
+            print("An item has been deleted")
             for items in myList:
-                 print("-", items)
-
-        elif user_input == "b":
-            itemName = input("Enter the item you want to remove: ")
-            if itemName in myList:
-                myList.remove(itemName)
-                print("="*57)
-                print("An item has been deleted")
-                for items in myList:
-                    print("-", items)
-            else:
-                print("="*57)
-                print("Item not found.")
-
-        elif user_input == "c":
-            itemName = input("Enter your chosen item here: ")
-            if itemName in myList:
-                new_item = input("What item do you want to modify?: ")
-                index = myList.index(itemName)
-                myList[index] = new_item
-                print("="*57)
-                print("List updated!")
-                for items in myList:
-                    print("-", items)
-            else:
-                print("="*57)
-                print("Item not found.")
-
-        elif user_input == "d":
-            itemName = input("Enter the item in the list that you want to count: ")
-            if itemName in myList:
-                counter = myList.count(itemName)
-                print("="*57)
-                print("The number of item is:")
-                print(counter)
-            else:
-                print("="*57)
-                print("Item not found.")
-
-        elif user_input == "e":
-                exit = input("\nAre you sure you want to exit the program?(y/n): ").lower()
-                if exit == "y":
-                    program = False
-                    print("\nThank you for using this program!")
-                    break
-
+                print("-", items)
         else:
-            print("Invalid number, please try again")
+            print("="*57)
+            print("Item not found.")
+
+    elif user_input == "c":
+        itemName = input("Enter your chosen item here: ")
+        if itemName in myList:
+            new_item = input("What item do you want to modify?: ")
+            index = myList.index(itemName)
+            myList[index] = new_item
+            print("="*57)
+            print("List updated!")
+            for items in myList:
+                print("-", items)
+        else:
+            print("="*57)
+            print("Item not found.")
+
+    elif user_input == "d":
+        itemName = input("Enter the item in the list that you want to count: ")
+        if itemName in myList:
+            counter = myList.count(itemName)
+            print("="*57)
+            print("The number of item is:")
+            print(counter)
+        else:
+            print("="*57)
+            print("Item not found.")
+
+    elif user_input == "e":
+            exit = input("\nAre you sure you want to exit the program?(y/n): ").lower()
+            if exit == "y":
+                program = False
+                print("\nThank you for using this program!")
+                break
+
+    else:
+        print("Invalid number, please try again")
