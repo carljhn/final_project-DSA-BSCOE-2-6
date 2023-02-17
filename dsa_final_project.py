130d4c77495c10dc93a9c567ee8a50a25cc1895f
Carljhn <zoletacarljohn1@gmail.com>
2023-02-17 22:28:04 +0800

    Initial commit: Commented instructions

---
+++ dsa_final_project.py
@@ -0,0 +1,7 @@
+# Pick any python project available on the internet that used "Data Structures and Algorithms" concept.
+# Set it up and make it work on your pc.
+# Push the code in your github before you do any changes.
+# Make some modifications to the project. Any idea you want to add.
+# Make a demo highlighting used data structure and algorithm and your modifications
+# Push your code changes in github.
+# Final checking Feb 18. Recorded demo.
